71a6a7f40b62fc908d7dbc1396d7f32896c215aa
hashmi <hashmi@imrans-MacBook-Air.local>
2022-02-27 16:04:17 +0530

    Added DetailViewController

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by hashmi on 27/02/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
